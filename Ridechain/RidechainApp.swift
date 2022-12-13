d6d59fe680c40c3c495d6f8d0c3934e57f688cd4
Danya <danyaa.tu@gmail.com>
2022-12-13 16:05:28 +0300

    Graduation project

---
+++ Ridechain/RidechainApp.swift
@@ -0,0 +1,17 @@
+//
+//  RidechainApp.swift
+//  Ridechain
+//
+//  Created by Sara AlMezeini on 19/05/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct RidechainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
