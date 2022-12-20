a601e76b0e2fe0a868fb99d8859762658aa8e4e9
danah <danahaalmadi@gmail.com>
2022-12-20 20:44:45 +0300

    comments on how to use the colors

--- Ridechain/colors.swift
+++ Ridechain/colors.swift
@@ -5,15 +5,52 @@
 //  Created by Danah  on 20/12/2022.
 //
 
+
+
+
+
+
+
+/*
+ 
+ dgreen >> dark green
+ lgreen >> light green
+ dblue  >> dark blue
+ lblue  >> light blue
+ slblue >> super light blue
+ 
+ 
+ 
+ 
+ 
+ 
+ you can use them like that:
+ 
+   1-    .foregroundColor(colorp.lblue)
+ 
+   and for backgrounds:
+   2-    .background(colorp.lblue)
+ 
+ 
+ 
+ */
+
+
+
+
+
+
+
+
 import Foundation
 import SwiftUI
 
 struct colorp{
-    static let dgreen = Color("dgreen")
-    static let lgreen = Color("lgreen")
-    static let lblue = Color("lblue")
-    static let dblue = Color("dblue")
-    static let slblue = Color("slblue")
+    static let dgreen = Color("dgreen") //dark green
+    static let lgreen = Color("lgreen") //light green
+    static let lblue = Color("lblue") //light blue
+    static let dblue = Color("dblue") //dark blue
+    static let slblue = Color("slblue")// super light blue
    
    
     
