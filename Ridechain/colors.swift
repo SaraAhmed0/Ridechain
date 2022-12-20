2e9ceebe6f00dc4d040116633aabeccbe0a1582b
danah <danahaalmadi@gmail.com>
2022-12-20 20:27:47 +0300

    just added the color palette

---
+++ Ridechain/colors.swift
@@ -0,0 +1,21 @@
+//
+//  colors.swift
+//  Ridechain
+//
+//  Created by Danah  on 20/12/2022.
+//
+
+import Foundation
+import SwiftUI
+
+struct colorp{
+    static let dgreen = Color("dgreen")
+    static let lgreen = Color("lgreen")
+    static let lblue = Color("lblue")
+    static let dblue = Color("dblue")
+    static let slblue = Color("slblue")
+   
+   
+    
+    
+}
